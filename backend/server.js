cbdcdced67664c01694f5b65b225d1293072e9de
karthiksharma22 <151902673+karthiksharma22@users.noreply.github.com>
2024-08-29 15:08:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://bahugun0042:Abc_123@clustertest.ilcjbdc.mongodb.net/?retryWrites=true&w=majority"
+    ""
   )
   .then(() => {
     console.log("Connected.");
